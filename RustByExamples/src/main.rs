af63804b668e3217e64a8fc9412f039e6ac27ca6
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2024-01-15 18:16:02 -0300

    .

--- RustByExamples/src/main.rs
+++ RustByExamples/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 mod formated_prints;
 mod primitive_types;
 mod custom_type;
@@ -10,4 +13,14 @@ fn main() {
 
     formated_prints::test();
     primitive_types::tuples();
+
+    // Recover Errors, panic!
+
+    let greeting_file = File::open("hello_world.txt").unwrap_or_else(|error| match error.kind() { // Tratamento de erro
+        ErrorKind::NotFound => match File::create("hello_world.txt") {
+            Ok(file) => file,
+            Err(error) => panic!("Problem creating the file: {:?}", error),
+        },
+        other_error => panic!("Problem opening the file: {:?}", error)
+    });
 }
